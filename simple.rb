47c9401a4c1468a86b4040aa2642a0c8daa85420
Salvo Virga <salvo.virga@tum.de>
2019-06-18 23:14:25 +0200

    fixed formula version

--- simple.rb
+++ simple.rb
@@ -3,6 +3,7 @@ class Simple < Formula
   homepage "https://github.com/IFL-CAMP/simple"
   url "https://github.com/IFL-CAMP/simple/releases/download/v.1.1.0/simple-1.1.0-Darwin-AppleClang8.tar.gz"
   sha256 "999d9ee9d3849e66b18fd60b1ca02a415b6635b5f0bcf84500c2e078ad375ba2"
+  version "1.1.0"
 
   def install
     lib.install "lib"
